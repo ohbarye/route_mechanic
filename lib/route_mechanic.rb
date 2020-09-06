2ed83eae9eefb02cbd784303f32557c1e5d3f529
ohbarye <over.rye@gmail.com>
2020-09-06 17:30:54 +0900

    bundle gem route_mechanic

---
+++ lib/route_mechanic.rb
@@ -0,0 +1,6 @@
+require "route_mechanic/version"
+
+module RouteMechanic
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/route_mechanic/version.rb
@@ -0,0 +1,3 @@
+module RouteMechanic
+  VERSION = "0.1.0"
+end
