aa9cbe88e9d59f154cc26bc8101b702a2d255a8f
ohbarye <over.rye@gmail.com>
2020-09-26 17:44:40 +0900

    Add new matchers to test for each purpose

--- lib/route_mechanic/rspec/matchers.rb
+++ lib/route_mechanic/rspec/matchers.rb
@@ -1,10 +1,20 @@
 require 'route_mechanic/rspec/matchers/have_valid_routes'
+require 'route_mechanic/rspec/matchers/have_no_unused_actions'
+require 'route_mechanic/rspec/matchers/have_no_unused_routes'
 
 module RouteMechanic
   module RSpec
     module Matchers
       def have_valid_routes(application=Rails.application)
         HaveValidRoutes.new(application)
+      end
+
+      def have_no_unused_actions(application=Rails.application)
+        HaveNoUnusedActions.new(application)
+      end
+
+      def have_no_unused_routes(application=Rails.application)
+        HaveNoUnusedRoutes.new(application)
       end
     end
   end
