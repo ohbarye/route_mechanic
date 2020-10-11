81a8f7c89659b307e749a42b9c460e2cc7b5fdb2
ohbarye <over.rye@gmail.com>
2020-10-11 15:16:06 +0900

    Add FakeEngine routes and controller for testing

--- fixtures/fake_app/lib/engine.rb
+++ fixtures/fake_app/lib/engine.rb
@@ -3,3 +3,19 @@ module FakeEngine
     isolate_namespace FakeEngine
   end
 end
+
+FakeEngine::Engine.routes.draw do
+  resources :fakes do
+    get 'hello', to: :hello
+  end
+end
+
+module FakeEngine
+  class ApplicationController < ActionController::Base; end
+end
+
+module FakeEngine
+  class FakesController < ApplicationController
+    def hello; end
+  end
+end
